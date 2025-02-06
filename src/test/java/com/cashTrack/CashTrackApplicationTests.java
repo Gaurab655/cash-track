86124193dd7c501b9255e04298c8af92274263a2
Gaurab655 <gaurabchand655@gmail.com>
2025-02-06 16:00:26 +0545

    Cash track

---
+++ src/test/java/com/cashTrack/CashTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cashTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
